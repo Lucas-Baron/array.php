be266a682fda60f9368776c5c28f452a8eb9e17e
Lucas Baron <lucasbaron28@hotmail.com>
2020-08-17 18:07:18 -0300

    Exercicio array

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$ingredientes = [
+    'açúcar',
+    'farinha',
+    'ovo',
+    'leite',
+    'fermento em pó'
+ ];
+
+ $numeroDaMegaSena = [
+     '29',
+     '45',
+     '33',
+     '21',
+     '07',
+     '44'
+ ];
+
+echo $ingredientes[2]."<br/>";
+echo "Quarto número sorteado: ".$numeroDaMegaSena[3];
